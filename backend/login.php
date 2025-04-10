3759bff3b92539a15269a9927ba361fd41d335e8
Brittany Ritter <b_ritter@mail.fhsu.edu>
2025-04-10 17:08:38 -0500

    edit

--- backend/login.php
+++ backend/login.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['role'] = $user['role'];
 
         // Redirect to dashboard
-        header("Location: dashboard.php");
+        header("Location: ../backend/dashboard.php");
         exit; 
     } else {
         $error_message = "Invalid credentials! Please try again.";
